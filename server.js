154bbb50cd9745511418214d46339a6b98672fe5
jay-rt <thapa.jayrt@gmail.com>
2022-11-20 10:24:43 -0500

    Seperated auth and post server

--- server.js
+++ server.js
@@ -35,11 +35,4 @@ app.get("/posts", authenticateToken, (req, res) =>
   res.json(posts.filter((post) => post.username === req.user.name))
 );
 
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
-});
-
 app.listen(3000);
